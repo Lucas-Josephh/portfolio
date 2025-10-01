f6a2c6a8e7781766b57a5736f385cb0f5099d74d
lucas <lucas.joseph@imerir.com>
2025-10-01 17:02:42 +0200

    Structure de la page

--- mon-portfolio/src/app/component/about/about.component.ts
+++ mon-portfolio/src/app/component/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { BookComponent } from '../../shared/book/book.component';
 
 @Component({
   selector: 'app-about',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, BookComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
